ca8f1a8e72c25a339d044ebd7f0dba8c7fabc862
Jerry <126196933+Jerryluis10@users.noreply.github.com>
2023-05-07 12:41:40 -0700

    Add files via upload

---
+++ transaction.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        .box{
+            padding: 10px;
+            margin-top: 25%;
+            margin-left: 11%;
+            border: 2px solid #008000;
+            padding-bottom: 25px;
+            width: 73%;
+        }
+        h2{
+            text-align: center;
+            font-size: bold;
+        }
+        h3{
+            text-align: center;
+        }
+        a{
+            width: 30%;
+            margin-left: 35%;
+            background-color: #008000;
+            color: white;
+            padding: 10px;
+            border-radius: 7px;
+            text-decoration: none;
+        }
+    </style>
+    <div class="box">
+        <h2>*12345#</h2>
+        <h3>If transaction is done</h3>
+        <a href="tracker-activation.html">Click here</a>
+    </div>
+</body>
+</html>
